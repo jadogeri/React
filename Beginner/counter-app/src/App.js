837f39924edd9b8c7c9150c646cbabfe85e56f42
OrleansParishIT <joseph.adogeri@orleansassessors.com>
2023-11-09 14:54:47 -0600

    fixed counterReducer.js

--- Beginner/counter-app/src/App.js
+++ Beginner/counter-app/src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
+import React,{useReducer} from 'react';
 import './App.css';
 import Button from './components/Button/Button';
+import counterReducer from './reducers/counterReducer';
+import { onClickHandler } from './utils/clickHandler';
+import Count from './components/Count/Count';
 
+const defaultState = {count : 0}
 function App() {
+  const [state, dispatch] = useReducer(counterReducer,defaultState)
+  console.log("count = "+ state.count)
+
   return (
     <div className="App">
-      <header className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p> */}
+      <header >   
+        <h1 className='app-title'>COUNTER APP</h1>
      
       </header>
-      <body>
-        <Button color={'red'}/>
-        <Button color={'blue'}/>
-        <Button color={'green'}/>
-
-
-
-      </body>
+        <div className='container'>
+        <h1 className='count-label'>COUNT</h1>
+        <Count value={state.count}/>
+        </div>
+        <div className='button-container'>
+        <Button color='red' title='DECREMENT' onClickHandler={()=>{onClickHandler(dispatch,{type :"DECREMENT"})}}/>
+        <Button color='skyblue' title='RESET' onClickHandler={()=>{onClickHandler(dispatch,{type :"RESET"})}}/>
+        <Button color='green'  title='INCREMENT' onClickHandler={()=>{onClickHandler(dispatch,{type :"INCREMENT"})}}/>
+    
+        </div>
     </div>
   );
 }
--- Beginner/counter-app/src/components/Button/Button.js
+++ Beginner/counter-app/src/components/Button/Button.js
@@ -1,13 +1,18 @@
 import React from 'react'
+import './Button.css'
 
-function Button() {
+function Button(props) {
+  const color = props.color;
+  const title = props.title;
+  const onClickHandler = props.onClickHandler;
+
+
   return (
     <div>
-        <button title="Button"/>
-        THIS IS A BUTTON
-      
+        <button onClick={onClickHandler} type='button'  style={{backgroundColor:color}}>{title}</button>
+             
     </div>
   )
 }
 
-export default Button
+export default Button;
